09afaacafc58b2e1ba6b43f1bd88a538c8d9d8cc
Mizzlsolti <149230144+Mizzlsolti@users.noreply.github.com>
2026-03-17 16:07:04 +0100

    Update current version from 2.4 to 2.5

--- Achievement_watcher.py
+++ Achievement_watcher.py
@@ -3950,7 +3950,7 @@ class MainWindow(QMainWindow, CloudStatsMixin):
         self._reset_status_label()
 
     def _check_for_updates(self):
-        CURRENT_VERSION = "2.4"
+        CURRENT_VERSION = "2.5"
         
         def _task():
             try:
